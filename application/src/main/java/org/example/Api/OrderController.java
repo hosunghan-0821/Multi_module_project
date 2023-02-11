c792da795cae15c828e01ff320e745a6158207f1
hosung <winsomed96@naver.com>
2023-02-11 15:24:20 +0900

    feat : test  rebase

--- application/src/main/java/org/example/Api/OrderController.java
+++ application/src/main/java/org/example/Api/OrderController.java
@@ -1,8 +1,14 @@
 package org.example.Api;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+
+    @GetMapping("/test")
+    public String testApi(){
+        return "hello";
+    }
 }
