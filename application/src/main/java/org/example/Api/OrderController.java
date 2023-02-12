2cef8ab850a66a9aebed2920155c7f2130fb76e0
hosung <winsomed96@naver.com>
2023-02-12 22:54:31 +0900

    test commit 2

--- application/src/main/java/org/example/Api/OrderController.java
+++ application/src/main/java/org/example/Api/OrderController.java
@@ -8,22 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @GetMapping("/test")
-    public String testApi(){
+    public String testApi() {
         return "hello";
     }
 
-
     public void test(){
         test1234();
-    }
-
-    public void  test1234(){
 
     }
 
-    public  void test12345(){
+    public void test1234() {
 
-        int a = 0 ;
+    }
+
+    public void test12345() {
+
+        int a = 0;
+        int b = 0;
     }
 
 
